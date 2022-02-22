b797e73c80ca352289115ba84616b5656dfe554f
Ode42 <ode.progr@outlook.com>
2022-02-22 16:11:58 +0200

    Pygame init

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planer Simulation")
+WHITE = (255, 255, 255)
+
+class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.6742e-11
+    SCALE = 250 / AU
+    TIMESTEP = 3600 * 24
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.sun = False
+        self.distance_to_sun = 0
+        self.orbit = []
+
+        self.x_vel = 0
+        self.y_vel = 0
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+main()
